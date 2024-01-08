0268d606a21fc20c3c5146dfd1f91f94ba1a8bc6
Akerele Adebayo <jubrilake@gmail.com>
2024-01-08 10:02:38 -0800

    updated

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,21 @@
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    else:
-        new_list = my_list[:]
-        new_list[idx] = element
-        return new_list
+    """Replace an element in a list at a specific location
+      without modifying the original
+
+    Args:
+        my_list: a list
+        idx: the index of item to replace
+        element: item to be substituted
+
+    Returns:
+        the edited list copy
+    """
+
+    temp_list = my_list.copy()
+    if idx < 0:
+        return temp_list
+    if idx >= len(my_list):
+        return temp_list
+    temp_list[idx] = element
+    return temp_list
