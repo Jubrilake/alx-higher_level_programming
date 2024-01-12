3ae21d3ca59aa21f5c313c63295b4be8a0109c62
Akerele Adebayo <jubrilake@gmail.com>
2024-01-12 04:02:21 -0800

    updated

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 def complex_delete(a_dictionary, value):
-    for key in a_dictionary.copy():
+    for key in list(a_dictionary):
         if a_dictionary[key] == value:
-            a_dictionary.pop(key)
-        return a_dictionary
+            del a_dictionary[key]
+    return a_dictionary
